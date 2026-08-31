65c0d0e23163a26574f76f9312db728f7ab98287
Steven Cao <stevencao258@gmail.com>
2026-08-31 13:09:21 -0700

    Reconfigured tile_renderer
    Added notes to Grid_Memory module to see where to work towards

--- Grid_Memory.sv
+++ Grid_Memory.sv
@@ -1,9 +1,54 @@
 module Grid_Memory(
 	input logic clk,
 	
-	input logic read,
-	output logic write
+	//PORT A - Game FSM
+	input logic write_enable,
+	input logic [4:0] fsm_row,
+	input logic [3:0] fsm_col,
+	input logic [3:0] fsm_write_data,
+	output logic [3:0] fsm_read_data,
+	
+	input logic [4:0] vga_row,
+	input logic [3:0] vga_col,
+	output logic [3:0] vga_read_data
 
 );
 
 endmodule
+
+
+/*
+//Initialize grid that acts like "memory" to store tiles and their colors. 
+logic [3:0] grid [0:19][0:9]; //2D Array colxrow 10x20 of 4 bits, (Temporary until grid memory is in FSM)
+
+
+//Initialize the memory grid with all black tiles
+initial begin
+	for(int r = 0; r < 20; r++) begin
+		for(int c = 0; c < 10; c++) begin
+			grid[r][c] = 4'b0000;
+		end
+	end
+	
+
+end
+
+	//Hard coding colored tiles
+	assign grid[19][0] = 4'd1; //Cyan
+	assign grid[19][1] = 4'd2; //Yellow
+	assign grid [10][5] = 4'd3; //Purple //Upside down
+	
+
+
+*/
+
+/*
+Steps to take:
+
+Tile Renderer: Remove grid memory and make it so it takes in color values from grid memory and then wrties through the case switch
+Figure out what module to calculate vga row and col
+
+Game FSM: Uses Write_enable toggles whether FSM is writing or reading the data. Then 4 bit data is sent into the 2d array or outputted when read
+
+
+*/
--- Tile_Renderer.sv
+++ Tile_Renderer.sv
@@ -3,47 +3,32 @@ module Tile_Renderer(
 	input logic [9:0] x_pos, // 800 pixels
 	input logic [9:0] y_pos, // 525 pixels
 	input logic video_active,
+	input logic [3:0] vga_read_data,
 	
 	output logic [3:0] RedDisplay,
 	output logic [3:0] GreenDisplay,
-	output logic [3:0] BlueDisplay
-
+	output logic [3:0] BlueDisplay,
+	
+	output logic [3:0] vga_col,
+	output logic [4:0] vga_row
 
 );
 
-//Initialize grid that acts like "memory" to store tiles and their colors. 
-logic [3:0] grid [0:19][0:9]; //2D Array colxrow 10x20 of 4 bits, (Temporary until grid memory is in FSM)
 
 //Initialize variables to represent when the line being rendered is within the playfield and what col/row it is, in the memory grid
 logic in_playfield;
 logic [3:0] col;
 logic [4:0] row;
 
-//Initialize the memory grid with all black tiles
-initial begin
-	for(int r = 0; r < 20; r++) begin
-		for(int c = 0; c < 10; c++) begin
-			grid[r][c] = 4'b0000;
-		end
-	end
-	
-
-end
-
-	//Hard coding colored tiles
-	assign grid[19][0] = 4'd1; //Cyan
-	assign grid[19][1] = 4'd2; //Yellow
-	assign grid [10][5] = 4'd3; //Purple //Upside down
-	
-//Logic used to determine whether the vga signal is within the designated play area. Outputs 1 when the signal is between horiontal pixel 76 to 256, then vertical pixel 5 to 415 during display lengths
-assign in_playfield =  (x_pos >= 220 && x_pos < 420) && (y_pos >= 40 && y_pos < 440); //200x400 pixel play area
-
+assign vga_col = col;
+assign vga_row = row;
 
 //Calculate the column and row in the memory grid you're currently traversing, in order to continue outputing stored tile color | Uses truncation 
 assign col = (x_pos - 220) / 20; // pixel position - offset from beginning of play area to find column position
 assign row = (y_pos - 40) / 20;
 
-
+//Logic used to determine whether the vga signal is within the designated play area. Outputs 1 when the signal is between horiontal pixel 76 to 256, then vertical pixel 5 to 415 during display lengths
+assign in_playfield =  (x_pos >= 220 && x_pos < 420) && (y_pos >= 40 && y_pos < 440); //200x400 pixel play area
 
 //Color logic
 always_comb begin
@@ -54,7 +39,7 @@ always_comb begin
 		
 	end else if (in_playfield) begin //If within the playfield begin displaying color ouputs based on the block information stored in each array slot
 		
-		case(grid[row][col]) //Each case is a 4 bit value that represents a color output for RGB display
+		case(vga_read_data) //Each case is a 4 bit value that represents a color output for RGB display
 			4'd0: {RedDisplay, GreenDisplay, BlueDisplay} = 12'h000; //Black Empty
 			4'd1: {RedDisplay, GreenDisplay, BlueDisplay} = 12'h0FF; // Cyan
 			4'd2: {RedDisplay, GreenDisplay, BlueDisplay} = 12'hFF0; // Yellow
